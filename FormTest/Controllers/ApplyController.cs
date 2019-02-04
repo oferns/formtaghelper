f9aaedb9eaa036de9c722b514ed7cbc85038aad9
Ollie J. Ferns <ollie@hq.nlsltd.com>
2019-02-04 14:10:05 +0000

    PoC Demo

---
+++ FormTest/Controllers/ApplyController.cs
@@ -0,0 +1,20 @@
+﻿
+namespace FormTest.Controllers {
+    using FormTest.Models;
+    using Microsoft.AspNetCore.Mvc;
+
+
+    [Area("Products")]
+    public class ApplyController : Controller {
+
+        [HttpGet]
+        public IActionResult Index(int eid) {
+            return View("Products", new ProductViewModel { ProductId = eid });
+        }
+
+        [HttpPost]
+        public IActionResult Index(int eid, ProductViewModel model) {
+            return View("Products");
+        }
+    }
+}
---
+++ FormTest/Models/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace FormTest.Models {
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+
+    public class ProductViewModel {
+
+        public int ProductId { get; set; }
+    }
+}
